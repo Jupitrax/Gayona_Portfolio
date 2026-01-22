c06b5d5b8ced2591f7038e6c3959b5b753fa77cb
JOHN ANDREI TORRENDON GAYONA <2100666@cc.usls.edu>
2026-01-22 14:06:41 +0800

    Lab Activity 1: Routes, Middleware, & Blade Layouts

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,8 @@
-<h1>Welcome to my Portfolio</h1>
+@extends('layouts.content')
+
+@section('title', 'Home')
+
+@section('content')
+<h1>Welcome to My Portfolio</h1>
+<p> Andrei wuz here.</p>
+@endsection
